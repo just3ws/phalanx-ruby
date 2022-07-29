07283bd68e0f014427c28d3fd0c24aa63f5c9969
Mike Hall <mike@just3ws.com>
2022-07-29 18:50:47 -0500

    Init

---
+++ lib/phalanx.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "phalanx/version"
+
+module Phalanx
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/phalanx/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Phalanx
+  VERSION = "0.1.0"
+end
