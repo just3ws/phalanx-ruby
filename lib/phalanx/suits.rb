7277b2cec063e2f654378aef8feb544c170ceff7
Mike Hall <mike@just3ws.com>
2022-08-02 19:20:31 -0500

    Null character and letters

--- lib/phalanx/suits.rb
+++ lib/phalanx/suits.rb
@@ -2,21 +2,33 @@
 
 module Phalanx
   module Suits
-    CLUB    = '♣'
-    DIAMOND = '♦'
-    HEART   = '♥'
-    SPADE   = '♠'
-    NULL    = '∅'
+    CLUB_GLYPH = '♣'
+    CLUB_LETTER = 'C'
 
-    REAL = [CLUB, DIAMOND, HEART, SPADE].freeze
-    ALL = [CLUB, DIAMOND, HEART, SPADE, NULL].freeze
+    DIAMOND_GLYPH = '♦'
+    DIAMOND_LETTER = 'D'
 
-    def self.null = NULL
-    def self.club = CLUB
-    def self.diamond = DIAMOND
-    def self.heart = HEART
-    def self.spade = SPADE
-    def self.real = REAL
-    def self.all = ALL
+    HEART_GLYPH = '♥'
+    HEART_LETTER = 'H'
+
+    NULL_GLYPH = '∅'
+    NULL_LETTER = 'N'
+
+    SPADE_GLYPH = '♠'
+    SPADE_LETTER = 'S'
+
+    ALL_GLYPHS = [CLUB_GLYPH, DIAMOND_GLYPH, HEART_GLYPH, SPADE_GLYPH, NULL_GLYPH].freeze
+    ALL_LETTERS = [CLUB_LETTER, DIAMOND_LETTER, HEART_LETTER, SPADE_LETTER, NULL_LETTER].freeze
+
+    REAL_GLYPHS = [CLUB_GLYPH, DIAMOND_GLYPH, HEART_GLYPH, SPADE_GLYPH].freeze
+    REAL_LETTERS = [CLUB_LETTER, DIAMOND_LETTER, HEART_LETTER, SPADE_LETTER].freeze
+
+    def self.null = NULL_GLYPH
+    def self.club = CLUB_GLYPH
+    def self.diamond = DIAMOND_GLYPH
+    def self.heart = HEART_GLYPH
+    def self.spade = SPADE_GLYPH
+    def self.real = REAL_GLYPHS
+    def self.all = ALL_GLYPHS
   end
 end
